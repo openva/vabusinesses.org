7c44f5f0eba780aca4d6238b6a8920e87f47899e
Waldo Jaquith <waldo@jaquith.org>
2014-09-12 23:39:10 -0400

    Remove useless HTML comment

--- index.php
+++ index.php
@@ -186,7 +186,6 @@ class Businesses
 
 		<script src="js/main.js"></script>
 
-		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 		<script>
 			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
@@ -197,4 +196,3 @@ class Businesses
 		</script>
 	</body>
 </html>
-
