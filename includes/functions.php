e1948c80bde330a4098f01e10d91d8faf287eed8
Waldo Jaquith <waldo@jaquith.org>
2014-09-15 23:15:34 -0400

    Move human_filesize() into an include

--- includes/functions.php
+++ includes/functions.php
@@ -11,8 +11,19 @@ function __autoload_libraries($class_name)
 	{
 		$result = include_once $filename;
 	}
+	
 	return;
 
 }
 
 spl_autoload_register('__autoload_libraries');
+
+/*
+ * Calculate file size in human-readable terms.
+ */
+function human_filesize($bytes, $decimals = 0)
+{
+	$sz = 'BKMGTP';
+	$factor = floor((strlen($bytes) - 1) / 3);
+	return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+}
