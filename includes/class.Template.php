878515f886708a52a76bf7ece7dbacd755d7d7e8
Waldo Jaquith <waldo@jaquith.org>
2014-09-15 23:15:16 -0400

    Start moving HTML generation into a template

---
+++ includes/class.Template.php
@@ -0,0 +1,289 @@
+<?php
+
+/**
+ * The Page class, for rendering HTML and delivering it to the browser
+ *
+ * PHP version 5
+ *
+ * @license		http://www.gnu.org/licenses/gpl.html GPL 3
+ * @version		0.8
+ * @link		http://www.statedecoded.com/
+ * @since		0.1
+ *
+ * Since all rendering occurs in-class, you can override the rendering method
+ * in your theme's Page class to use a totally different rendering engine (Twig,
+ * Smarty, or straight PHP) if you'd like!
+ *
+ */
+
+/**
+ * Turn the variables provided by each page into a rendered page.
+ */
+class Page
+{
+
+	public $html;
+	public $page = 'default';
+	public $template_file = '';
+	public $theme_name = '';
+	public $theme_dir = '';
+	public $theme_web_path = '';
+	public $static_path = '';
+
+	public $assets = array(
+		/* 'asset_name' => array(
+				'path' => '/relative/path/to/file.css',
+				'resolved_path' => '/absolute/path/to/file.css', // This is generated.
+				'type' => 'stylesheet', // or javascript or whatever.
+				'requires' => array('first_asset_name', 'second_asset_name')) */
+	);
+
+	public function __construct($page=null)
+	{
+	
+		/*
+		 * Set defaults
+		 */
+		if (strlen($this->theme_name) === 0)
+		{
+			$this->theme_name = 'basic';
+		}
+
+		/*
+		 * Pre-parse assets to set the full path.
+		 */
+		$this->add_assets($this->assets);
+
+		if (isset($page))
+		{
+			$this->page = $page;
+		}
+		$this->template_file = $this->theme_dir . $this->page . '.inc.php';
+
+		$this->html = $this->load_template($this->template_file);
+		
+	}
+
+	/**
+	 * Get our template data
+	 */
+	public function load_template($template_file)
+	{
+	
+		/*
+		 * Save the contents of the template file to a variable. First check Memcached and see if
+		 * it's stored there, if we've enabled the use of Memcached.
+		 */
+		$storage_name = 'template-' . $this->theme_name . '-' . $this->page;
+		if ( MEMCACHED === TRUE)
+		{
+		
+			$html = $mc->get($storage-name);
+			if ($html != FALSE)
+			{
+				$html = unserialize($html);
+			}
+			else
+			{
+				
+				if (file_exists($template_file))
+				{
+					$html = file_get_contents($template_file);
+				}
+				
+				/*
+				 * Cache the template for one day.
+				 */
+				$mc->set($storage_name, serialize($html), 86400);
+				
+			}
+		}
+		else
+		{
+			$html = file_get_contents($template_file);
+		}
+
+		return $html;
+		
+	}
+
+
+	/**
+	 * A shortcut for all steps necessary to turn variables into an output page.
+	 */
+	public function parse($content)
+	{
+		return $this->display($this->render($content));
+	}
+
+
+	/**
+	 * Combine the populated variables with the template.
+	 */
+	public function render($content)
+	{
+	
+		/*
+		 * Make a copy of the template here, so we can re-render as often as we like with new
+		 * content.
+		 */
+		$template = $this->html;
+
+		$this->before_render($template, $content);
+
+		/*
+		 * Replace all of our in-page tokens with our defined variables.
+		 */
+		foreach ($content->get() as $field=>$value)
+		{
+			$template = str_replace('{{' . $field . '}}', $value, $template);
+		}
+
+		$this->after_render($template, $content);
+
+		return $template;
+		
+	}
+
+
+	/**
+	 * Send the page to the browser.
+	 */
+	public function display($content)
+	{
+
+		if (!isset($content))
+		{
+			return FALSE;
+		}
+
+		echo $content;
+		return TRUE;
+
+	}
+
+
+	/**
+	 * Add a new asset.
+	 */
+	public function add_asset($name, $asset = array())
+	{
+	
+		/*
+		 * If we have a local path.
+		 */
+		if (substr($asset['path'], 0, 1) === '/' && substr($asset['path'], 0, 2) !== '//')
+		{
+			/*
+			 * Drop the leading slash.
+			 */
+			$asset['resolved_path'] = $this->static_path . substr($asset['path'], 1);
+		}
+		else
+		{
+			$asset['resolved_path'] = $asset['path'];
+		}
+
+		if(isset($requires))
+		{
+			$asset['requires'] = $requires;
+		}
+		$this->assets[$name] = $asset;
+		
+	}
+
+
+	/**
+	 * Add an array of assets.
+	 */
+	public function add_assets($assets)
+	{
+	
+		foreach ($assets as $name => $asset)
+		{
+			$this->add_asset($name, $asset);
+		}
+		
+	}
+
+
+	/**
+	 * Parse through assets and resolve dependencies.
+	 */
+	public function parse_assets()
+	{
+	
+		$collated_assets = array();
+
+		foreach ($this->assets as $name => $asset)
+		{
+			$this->build_asset($collated_assets, $name, $asset);
+		}
+
+		return $collated_assets;
+		
+	}
+
+
+	/**
+	 * Internal asset builder for final asset rendering
+	 * Edits $collated_assets in place!
+	 */
+	public function build_asset(&$collated_assets, $name, $asset)
+	{
+	
+		$type = $asset['type'];
+		
+		/*
+		 * Resolve requirements.
+		 */
+		if (isset($asset['requires']))
+		{
+		
+			foreach ($asset['requires'] as $required)
+			{
+			
+				if (!in_array($required, array_keys($this->assets)))
+				{
+					throw new Exception('Required asset "' . $required .
+						'" not defined, from parent "' . $name .'"');
+				}
+				
+				else
+				{
+
+					/*
+					 * If the requirement has not yet been satisfied, we recurse to add it.
+					 */
+					$resolved_path = $this->assets[$required]['resolved_path'];
+					$asset_type = $this->assets[$required]['type'];
+					if(!isset($collated_assets[$type]))
+					{
+						$collated_assets[$type] = array();
+					}
+
+					if (!in_array(
+						$resolved_path,
+						$collated_assets[$type]))
+					{
+						$collated_assets = $this->build_asset($collated_assets, $required,
+							$this->assets[$required]);
+					}
+					
+				} /* !in_array($required ... */
+				
+			} /* foreach ($asset['requires'] */
+			
+		} /* if (isset($asset['requires'] .. */
+
+		/*
+		 * The easy part - just add the asset.
+		 * We collate these into types, for ease of display.
+		 */
+		$collated_assets[ $type ][] = $asset['resolved_path'];
+
+		return $collated_assets;
+
+	} /* function build_asset */
+
+}
