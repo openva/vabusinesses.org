3c3477d5e8e17ab60a249694e004d3bc90d45f6c
Waldo Jaquith <waldo@jaquith.org>
2015-02-05 21:04:08 -0500

    Add export functionality

--- includes/class.Businesses.php
+++ includes/class.Businesses.php
@@ -41,7 +41,7 @@ class Businesses
 			'json'	=> '9_llc.json')
 	);
 	
-	/*
+	/**
 	 * List the names, dates, and size of all CSV and JSON files.
 	 */
 	function list_files()
@@ -61,5 +61,148 @@ class Businesses
 		return TRUE;
 		
 	}
+	
+	/**
+	 * Export all records for a given place.
+	 *
+	 * Requires $this->params, which is an elasticsearch-php-ready set of Elasticsearch parameters.
+	 * Returns TRUE/FALSE, echoing results directly to the browser.
+	 *
+	 * TO DO
+	 * - Add CSV support.
+	 */
+	function export_results()
+	{
+		
+		/*
+		 * We require Elasticsearch parameters.
+		 */
+		if (!isset($this->params))
+		{
+			return FALSE;
+		}
+		
+		/*
+		 * Only supports JSON at the moment.
+		 */
+		if (!isset($this->format))
+		{
+			$this->format = 'json';
+		}
+		
+		/*
+		 * Normally we limit the number of Elasticsearch results to something small, but we want
+		 * to be able to export a great many records here. So we set a high ceiling.
+		 */
+		$this->params['size'] = 100000;
+		
+		/*
+		 * Bring the Memcached connection into the scope of this method.
+		 */
+		global $mc;
+		
+		/*
+		 * Bring the Elasticsearch connection into the scope of this method.
+		 */
+		global $es;
+
+		/*
+		 * TO DO: Take the below out of this method. Both the table maps line, which should be its
+		 * own nethod, and also the sort order code, which I guess should also be its own method.
+		 */
+		$tables = unserialize($mc->get('table-maps'));
+		$sort_order = array();
+		foreach($tables as $table_number => $fields)
+		{
+			foreach ($fields as $field)
+			{
+				$sort_order[$table_number][] = $field['alt_name'];
+			}
+		}
+		
+		/*
+		 * Submit our query to Elasticsearch.
+		 */
+		$results = $es->search($this->params);
+
+		if ( ($results === FALSE) || ($results['hits']['total'] == 0) )
+		{
+			return FALSE;
+		}
+		
+		/*
+		 * If JSON has been requested.
+		 */
+		if ($this->format == 'json')
+		{
+			header('Content-Type: application/json');
+			echo '[';
+		}
+		
+		/*
+		 * Count loops, so that we don't append a comma after the final JSON element.
+		 */
+		$i=0;
+
+		/*
+		 * Walk through all of the results.
+		 */
+		foreach ($results['hits']['hits'] as $result)
+		{
+			
+			/*
+			 * Raise coordinates up a level in the array structure.
+			 */
+			if (isset($result['_source']['location']['coordinates']))
+			{
+				$result['_source']['coordinates'] = $result['_source']['location']['coordinates'];
+				unset($result['_source']['location']);
+			}
+		
+			/*
+			 * Rearrange the fields per the prescribed key order for this type of record.
+			 */
+			if (isset($sort_order[$result{'_type'}]))
+			{
+		
+				$ordered_result = array();
+				foreach ($sort_order[$result{'_type'}] as $key)
+				{
+					$ordered_result[$key] = $result['_source'][$key];	
+				}
+			
+				/*
+				 * Replace the result with the newly ordered result.
+				 */
+				$result['_source'] = $ordered_result;
+				unset($ordered_result);
+			
+			}
+		
+			if ($this->format == 'json')
+			{
+				echo json_encode($result['_source']);
+				if ( ($i+1) < $results['hits']['total'])
+				{
+
+					echo ',';
+				}
+			}
+
+			$i++;
+			
+		}
+		
+		/*
+		 * If JSON has been requested.
+		 */
+		if ($this->format == 'json')
+		{
+			echo ']';
+		}
+			
+		return TRUE;
+	
+	} // end export_results()
 
 }
