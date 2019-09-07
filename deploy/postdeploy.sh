aed800e2ce12eeb448eb01cb78e89e876572ed75
Waldo Jaquith <waldo@jaquith.org>
2019-09-07 14:04:58 -0400

    Optionally install SQLite extension

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -4,3 +4,8 @@ cd /vol/vabusinesses.org/htdocs/
 
 # Set up the crontab
 crontab deploy/crontab
+
+# Enable the SQLite extension
+if [ "$(dpkg -l |grep php |grep -c sqlite)" -lt 1 ]; then
+    apt-get install php5-sqlite
+fi
