84282940623cf1f0469114b77da0295d8a2f8883
Waldo Jaquith <waldo@jaquith.org>
2019-09-06 23:11:13 -0400

    Use the ID from the URL

--- api/business.php
+++ api/business.php
@@ -2,12 +2,20 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/autoloader.php';
 
+/*
+ * If there's a 7-character ID, use that.
+ */
+if ( isset($_GET['id']) && strlen($_GET['id']) == 7)
+{
+    $id = $_GET['id'];
+}
+
 $database = new Database;
 $db = $database->connect();
 
 $business = new Business;
 $business->db = $db;
-$business->id = '0530258';
+$business->id = $id;;
 $biz = $business->fetch();
 
 echo json_encode($biz);
