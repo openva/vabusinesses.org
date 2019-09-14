18aaa9849df44761ab69b2e2bdc9bd9194bdf70f
Waldo Jaquith <waldo@jaquith.org>
2019-09-13 21:31:58 -0400

    Add absurdly crude error-handling

--- api/business.php
+++ api/business.php
@@ -3,7 +3,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/autoloader.php';
 
 /*
- * If there's a 7-character ID, use that.
+ * If there's a 7-character ID, use that
  */
 if ( isset($_GET['id']) && strlen($_GET['id']) == 7)
 {
@@ -13,9 +13,24 @@ if ( isset($_GET['id']) && strlen($_GET['id']) == 7)
 $database = new Database;
 $db = $database->connect();
 
+if (!$db)
+{
+    echo json_encode('Error');
+    exit;
+}
+
+/*
+ * Get the business record
+ */
 $business = new Business;
 $business->db = $db;
 $business->id = $id;
 $biz = $business->fetch();
+
+if (!is_array($biz))
+{
+    echo json_encode('Error');
+    exit;
+}
 
 echo json_encode($biz);
