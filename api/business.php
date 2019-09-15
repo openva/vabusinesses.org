949eebb71eb15fa05b29e7cb511fd798d8ace674
Waldo Jaquith <waldo@jaquith.org>
2019-09-15 15:09:32 -0400

    Add HTTP headers to API errors

--- api/business.php
+++ api/business.php
@@ -3,7 +3,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/autoloader.php';
 
 /*
- * If there's a 7-character ID, use that
+ * Use the identifier passed in the URL
  */
 if ( isset($_GET['id']) && strlen($_GET['id']) == 7)
 {
@@ -15,6 +15,7 @@ $db = $database->connect();
 
 if (!$db)
 {
+    header($_SERVER["SERVER_PROTOCOL"]." 500 Internal Server Error", true, 500);
     echo json_encode('Error');
     exit;
 }
@@ -29,6 +30,7 @@ $biz = $business->fetch();
 
 if (!is_array($biz))
 {
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
     echo json_encode('Error');
     exit;
 }
