3cb771383dada3d420e68e2654561db1d7d3fe81
Waldo Jaquith <waldo@jaquith.org>
2019-09-15 20:12:17 -0400

    Switch to the deploy directory when running

--- deploy/populate-secrets.sh
+++ deploy/populate-secrets.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cd $(dirname "$0") || exit 1
+
 # We use an alternate regex delimiter because the webhook URL contains slashes
 sed -i "s|{SLACK_WEBHOOK_URL}|$SLACK_WEBHOOK|g" ../scripts/secrets.sh
