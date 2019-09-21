7e5417978a4e8133d3c861f2e95ae0e4adba0c7d
Waldo Jaquith <waldo@jaquith.org>
2019-09-21 14:07:39 -0400

    Add an API method to search business names
    Toward #85.

---
+++ api/search.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/autoloader.php';
+
+/*
+ * Use the search string passed in the URL
+ */
+if ( isset($_GET['query']) )
+{
+    $query = filter_var($_GET['query'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+}
+
+$database = new Database;
+$db = $database->connect();
+
+if (!$db)
+{
+    header($_SERVER["SERVER_PROTOCOL"]." 500 Internal Server Error", true, 500);
+    echo json_encode('Error');
+    exit;
+}
+
+/*
+ * Get the first 50 matching records
+ */
+$business = new Business;
+$business->db = $db;
+$business->query = $query;
+$results = $business->search();
+
+if (!is_array($results))
+{
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+    echo json_encode('Error');
+    exit;
+}
+
+echo json_encode($results);
--- includes/class.Business.php
+++ includes/class.Business.php
@@ -32,4 +32,36 @@ class Business
 
     }
 
+    /*
+     * Search matching business records, return the first 100
+     */
+    function search()
+    {
+
+        if (!isset($this->db) || !isset($this->query))
+        {
+            return FALSE;
+        }
+
+        $sql = 'SELECT *
+                FROM corp
+                WHERE Name LIKE "%' . $this->query . '%"
+                LIMIT 100';
+        $result = $this->db->query($sql);
+
+        if ($result->numColumns() == 0)
+        {
+            return false;
+        }
+
+        $this->results = [];
+        while ($business = $result->fetchArray(SQLITE3_ASSOC))
+        {
+            $this->results[] = $business;
+        }
+
+        return $this->results;
+
+    }
+
 }
