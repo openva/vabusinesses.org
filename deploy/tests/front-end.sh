4c7d37fd50b817076815dfad680f99bdfc5f8382
Waldo Jaquith <waldo@jaquith.org>
2019-09-25 17:42:49 -0400

    Add a front-end search test

--- deploy/tests/front-end.sh
+++ deploy/tests/front-end.sh
@@ -6,6 +6,12 @@ if $("curl -s http://localhost/business/F000032 |grep -c 'AMERICAN BRANDS'") -lt
     ERRORED=true
 fi
 
+# Run a search to verify that there are results
+if $("curl -s http://localhost/search.php?query=peabody |grep -c 'Riggs'") -lt 1; then
+    echo "ERROR: Search is not returning results"
+    ERRORED=true
+fi
+
 # If any tests failed, have this script return that failure
 if [ "$ERRORED" == true ]; then
     exit 1
