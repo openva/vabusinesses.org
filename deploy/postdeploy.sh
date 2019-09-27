86eb73127b56fc22340dca7f40511d19c8472155
Waldo Jaquith <waldo@jaquith.org>
2019-09-26 21:18:48 -0400

    Small enhancements to server config

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-cd /vol/vabusinesses.org/htdocs/deploy/
+cd /vol/vabusinesses.org/htdocs/deploy/ || exit
 
 # Set up the crontab
 crontab deploy
 
 # Enable the SQLite extension
 if [ "$(dpkg -l |grep php |grep -c sqlite)" -lt 1 ]; then
-    apt-get install php5-sqlite
+    apt-get install -y php5-sqlite
+fi
+
+# Enable the SQLite extension
+if [ "$(dpkg -l |grep npm)" -lt 1 ]; then
+    apt-get install -y npm
 fi
 
 # Save Travis CI secrets to a file
@@ -15,4 +20,5 @@ fi
 
 # Give the web server user ownership over all files
 cd ..
-chown www-data *
+chown www-data ./*
+chgrp ubuntu ./*
