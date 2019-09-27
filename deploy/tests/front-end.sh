14df327de8fe2b854e05ec80f330829ac377d64a
Waldo Jaquith <waldo@jaquith.org>
2019-09-26 21:20:05 -0400

    Fix Bash conditional formats

--- deploy/tests/front-end.sh
+++ deploy/tests/front-end.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Fetch a single business's records
-if $("curl -s http://localhost/business/F000032 |grep -c 'AMERICAN BRANDS'") -lt 1; then
+if [ "$(curl -s http://localhost/business/F000032 |grep -c 'AMERICAN BRANDS')" -lt 1 ]; then
     echo "ERROR: Front-end is not returning business records"
     ERRORED=true
 fi
@@ -13,7 +13,7 @@ if [ "$(curl -Is http://localhost/business/F000001 |grep -c '404 Not Found')" -l
 fi
 
 # Run a search to verify that there are results
-if $("curl -s http://localhost/search.php?query=peabody |grep -c 'Riggs'") -lt 1; then
+if [ "$(curl -s http://localhost/search.php?query=peabody |grep -c 'Riggs')" -lt 1 ]; then
     echo "ERROR: Search is not returning results"
     ERRORED=true
 fi
