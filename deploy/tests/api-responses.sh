a2b01bb9345569c59bde1fa239458930febe511e
Waldo Jaquith <waldo@jaquith.org>
2019-10-04 21:59:33 -0400

    Display full API response when a test fails

--- deploy/tests/api-responses.sh
+++ deploy/tests/api-responses.sh
@@ -5,21 +5,25 @@ BUSINESS_JSON="$(curl -s http://localhost/api/business/F000032)"
 
 if [ "$(echo $BUSINESS_JSON |jq '.EntityID')" != '"F000032"' ]; then
     echo "ERROR: API is not returning EntityID correctly"
+    echo $BUSINESS_JSON
     ERRORED=true
 fi
 
 if [ "$(echo $BUSINESS_JSON |jq '.Stock1')" != '"COMMON (200000000)"' ]; then
     echo "ERROR: API is not returning Stock1 correctly"
+    echo $BUSINESS_JSON
     ERRORED=true
 fi
 
 if [ "$(echo $BUSINESS_JSON |jq '.IncorpDate')" != '"1903-08-18"' ]; then
     echo "ERROR: API is not returning IncorpDate correctly"
+    echo $BUSINESS_JSON
     ERRORED=true
 fi
 
 if [ "$(echo $BUSINESS_JSON |jq '.Name')" != '"AMERICAN BRANDS, INC."' ]; then
     echo "ERROR: API is not returning Name correctly"
+    echo $BUSINESS_JSON
     ERRORED=true
 fi
 
@@ -28,6 +32,7 @@ SEARCH_JSON="$(curl -s http://localhost/api/search/test)"
 
 if [ "$(echo $SEARCH_JSON |jq '. | length')" -ne '100' ]; then
     echo "ERROR: API is not returning enough search results"
+    echo $SEARCH_JSON
     ERRORED=true
 fi
 
@@ -36,6 +41,7 @@ SEARCH_JSON="$(curl -s http://localhost/api/search/asdflasdfqasdl)"
 
 if [ "$(echo $SEARCH_JSON |jq '. | length')" -ne '0' ]; then
     echo "ERROR: API is not returning excessive search results"
+    echo $SEARCH_JSON
     ERRORED=true
 fi
 
