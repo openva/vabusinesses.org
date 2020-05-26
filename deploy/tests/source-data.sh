5bb2e2011e404863184c7bbaf005efcf38d5042f
Waldo Jaquith <waldo@jaquith.org>
2020-05-26 10:51:47 -0400

    Nest SQLite tests
    A whole bunch of tests fail — creating a lot of confusing noise —  if the SQLite file doesn't exist, so only run them conditionally.

--- deploy/tests/source-data.sh
+++ deploy/tests/source-data.sh
@@ -22,30 +22,32 @@ fi
 if [[ ! -e ../../data/vabusinesses.sqlite ]]; then
     echo "ERROR: SQLite file not found"
     ERRORED=true
-fi
+    
+else
+    # See if the right tables exist in SQLite
+    if [ "$(sqlite3 ../../data/vabusinesses.sqlite .tables |perl -pne 's/\s+/,/g')" != "amendment,llc,merger,officer,tables,corp,lp,name_history,reserved_name," ]; then
+        echo "ERROR: Unexpected list of SQLite tables created"
+        ERRORED=true
+    fi
 
-# See if the right tables exist in SQLite
-if [ "$(sqlite3 ../../data/vabusinesses.sqlite .tables |perl -pne 's/\s+/,/g')" != "amendment,llc,merger,officer,tables,corp,lp,name_history,reserved_name," ]; then
-    echo "ERROR: Unexpected list of SQLite tables created"
-    ERRORED=true
-fi
+    # See if we have a reasonable number of records in SQLite's corp table
+    if [ "$(sqlite3 ../../data/vabusinesses.sqlite 'SELECT COUNT(*) FROM corp')" -lt 350000 ]; then
+        echo "ERROR: Insufficient SQLite rows found for corporate data"
+        ERRORED=true
+    fi
 
-# See if we have a reasonable number of records in SQLite's corp table
-if [ "$(sqlite3 ../../data/vabusinesses.sqlite 'SELECT COUNT(*) FROM corp')" -lt 350000 ]; then
-    echo "ERROR: Insufficient SQLite rows found for corporate data"
-    ERRORED=true
-fi
+    # See if we have a reasonable number of records in SQLite's llc table
+    if [ "$(sqlite3 ../../data/vabusinesses.sqlite 'SELECT COUNT(*) FROM llc')" -lt 730000 ]; then
+        echo "ERROR: Insufficient SQLite rows found for llc data"
+        ERRORED=true
+    fi
 
-# See if we have a reasonable number of records in SQLite's llc table
-if [ "$(sqlite3 ../../data/vabusinesses.sqlite 'SELECT COUNT(*) FROM llc')" -lt 730000 ]; then
-    echo "ERROR: Insufficient SQLite rows found for llc data"
-    ERRORED=true
-fi
+    # See if we have a reasonable number of records in SQLite's officer table
+    if [ "$(sqlite3 ../../data/vabusinesses.sqlite 'SELECT COUNT(*) FROM officer')" -lt 650000 ]; then
+        echo "ERROR: Insufficient SQLite rows found for officers data"
+        ERRORED=true
+    fi
 
-# See if we have a reasonable number of records in SQLite's officer table
-if [ "$(sqlite3 ../../data/vabusinesses.sqlite 'SELECT COUNT(*) FROM officer')" -lt 650000 ]; then
-    echo "ERROR: Insufficient SQLite rows found for officers data"
-    ERRORED=true
 fi
 
 # If any tests failed, have this script return that failure
