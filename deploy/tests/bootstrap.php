44dc1686139ca2389c86a68ef6a915d793b52721
Waldo Jaquith <waldo@jaquith.org>
2020-05-26 17:08:59 -0400

    Define the server protcol properly

--- deploy/tests/bootstrap.php
+++ deploy/tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-define('SERVER_PROTOCOL', 'HTTP/1.1');
+$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
 $_SERVER['DOCUMENT_ROOT'] = str_replace( 'deploy/tests', '', __DIR__ );
 $_SERVER['SERVER_NAME'] = gethostname();
