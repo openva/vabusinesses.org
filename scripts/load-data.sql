582ce79e7cd8a551159bcffdb4613da82f9ae321
Waldo Jaquith <waldo@jaquith.org>
2023-08-30 22:24:34 -0400

    Create some indices

--- scripts/load-data.sql
+++ scripts/load-data.sql
@@ -8,3 +8,8 @@
 .import ../data/llc.csv llc
 .import ../data/merger.csv merger
 .import ../data/officer.csv officer
+CREATE INDEX corpIncorpDate ON corp (IncorpDate);
+CREATE INDEX corpName ON corp (Name);
+CREATE INDEX llcName ON llc (Name);
+CREATE INDEX lpName ON lp (Name);
+CREATE INDEX officerEntityId ON officer (EntityID);
